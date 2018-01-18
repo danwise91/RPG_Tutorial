a79a9716cac1a6c30ff1d74cd513f3490a6e438f
danwise91 <danielwise19918@gmail.com>
2018-01-18 18:14:08 -0500

    first

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour {
+    const float locomoationAnimationSmoothTime = .1f; 
+    Animator animator;
+    NavMeshAgent agent; 
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomoationAnimationSmoothTime, Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    public LayerMask movementMask;
+    PlayerMotor motor;
+    public Interactible focus; 
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return; 
+        }
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                //motor.MoveToPoint(hit.point);
+                //move our player to what we hit 
+
+                RemoveFocus();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+               Interactible interactible = hit.collider.GetComponent<Interactible>();
+               if (interactible != null)
+                {
+                    SetFocus(interactible);
+                }
+            }
+        }
+    }
+
+   void SetFocus(Interactible newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+       newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
